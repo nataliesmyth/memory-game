a08767ad6866fc4a0b54d5fdfe03105d8875646f
Nataliesmyth <nataliesmyth11@gmail.com>
2022-05-26 16:11:16 -0400

    flip card logic fix

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,7 @@
-console.log("sanity-check")
-
 const cards = document.querySelectorAll('.memory-card');
 
 function flipCard() {
-    this.classList.toggle('flip');
+  this.classList.toggle('flip');
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-console.log(cards)
