7dedb18ace73a329cf142d86a380d740774f7232
Nataliesmyth <nataliesmyth11@gmail.com>
2022-05-26 16:55:15 -0400

    added card reset if cards clicked do not match

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,61 @@
 const cards = document.querySelectorAll('.memory-card');
 
+// card state
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-  this.classList.toggle('flip');
+    if (lockBoard) return;
+    // prevent user from clicking the same card twice
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards();
+        return;
+    }
+
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    // call this method to reset variables after each round
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        // call method to reset board
+        resetBoard();
+    }, 1500)
+}
+
+// Method for resetting board after second card flip if cards do not match
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
